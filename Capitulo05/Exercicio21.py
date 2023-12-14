fce71ec05eff4ff3de2ba03d80a7ebe34d914469
JVSMOTA <jvmota.sb@gmail.com>
2023-12-14 08:53:06 -0300

    :sparkles: feat: Exercicio21 - Contador de cédulas aninhado

---
+++ Capitulo05/Exercicio21.py
@@ -0,0 +1,67 @@
+#==============================================================================#
+# Author: João Mota < jvmota.sb@gmail.com >                                    #
+# GitHub: https://github.com/JVSMOTA                                           #
+# Computer Science undergraduate at UFCG                                       #
+#==============================================================================#
+
+# Created: 14/12/2023
+# Time of Creation: 08:38:51
+#
+# Question: Reescreva o programa da listagem 5.14 de forma a continuar executando até que o valor digitado seja 0. Utilize repetições aninhadas.
+
+# Listagem 5.14 – Contagem de cédulas
+
+# valor=int(input("Digite o valor a pagar:"))
+# cédulas=0
+# atual=50
+# apagar=valor
+# while True:
+#     if atual<=apagar:
+#         apagar-=atual
+#         cédulas+=1
+#     else:
+#         print("%d cédula(s) de R$%d" % (cédulas, atual))
+#         if apagar == 0:
+#             break
+#         if atual == 50:
+#             atual = 20
+#         elif atual == 20:
+#             atual = 10
+#         elif atual == 10:
+#             atual = 5
+#         elif atual == 5:
+#             atual = 1
+#             cédulas = 0
+
+while True:
+    valor = int(input("Digite o valor a pagar (ou 0 para encerrar): "))
+    if valor == 0:
+        break
+    
+    cédulas = 0
+    atual = 100
+    apagar = valor
+    
+    while True:
+
+        if atual <= apagar:
+            apagar -= atual
+            cédulas += 1
+
+        else:
+            print("%d cédula(s) de R$%d" % (cédulas, atual))
+            if apagar == 0:
+                break
+
+            if atual == 100:
+                atual = 50
+            elif atual == 50:
+                atual = 20
+            elif atual == 20:
+                atual = 10
+            elif atual == 10:
+                atual = 5
+            elif atual == 5:
+                atual = 1
+
+            cédulas = 0
